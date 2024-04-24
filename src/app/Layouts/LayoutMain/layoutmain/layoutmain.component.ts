2b3d46328e3ae500d78bfcab853ecee14a69c95c
anthony <tony20402014@gmail.com>
2024-04-24 15:27:01 -0600

    base

---
+++ src/app/Layouts/LayoutMain/layoutmain/layoutmain.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layoutmain',
+  standalone: true,
+  imports: [],
+  templateUrl: './layoutmain.component.html',
+  styleUrl: './layoutmain.component.css'
+})
+export class LayoutmainComponent {
+
+}
---
+++ src/app/Views/MenuGame/menugame/menugame.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menugame',
+  standalone: true,
+  imports: [],
+  templateUrl: './menugame.component.html',
+  styleUrl: './menugame.component.css'
+})
+export class MenugameComponent {
+
+}
