66ed33324ab8c6a30e68fd0bffd1dc5d1065cddf
MarcoChavezB <marco1102004@gmail.com>
2024-04-24 16:23:07 -0600

    añadiendo login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './Layouts/auth/auth.component';
+import { LoginComponent } from './Views/Auth/login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: AuthComponent,
+        children:[
+            {
+                path: '',
+                component: LoginComponent
+            }
+        ]
     }
 ];
