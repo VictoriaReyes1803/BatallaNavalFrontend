e0b4cbecacb76a8e5598b5239a55f287ea9ae914
anthony <tony20402014@gmail.com>
2024-04-25 12:56:34 -0600

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        water: '#blue', // Cambia el color según prefieras
+        ship: '#gray',  // Cambia el color según prefieras
+        hited: '#red',  // Cambia el color según prefieras
+        missed: '#white'  // Cambia el color según prefieras
+      }
+    },
   },
   plugins: [],
 }
